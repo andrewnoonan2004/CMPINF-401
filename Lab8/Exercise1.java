2314eefb5805c1610c0f868fd62a64a760b7302b
Andrew <andrew@localhost.localdomain>
2024-03-28 14:55:22 -0400

    Lab8 finished

--- Lab8/Exercise1.java
+++ Lab8/Exercise1.java
@@ -1,54 +1,46 @@
-/*
-	Exercise1.java
-	
-	- WRITE A SINGLE DO LOOP THAT DOES THE FOLLOWING UNTIL THE USER ENTERS A VALID INPUT FILENAME
-	- PROMPT THE USER FOR A FILENAME AND USE TRY CATCH TO RE-PROMPT
-	- IF INPUT IS NOT AN INT YOU MUST CLERA THE BUFFER BECUASE THAT BOGUS STRING IS STILL IN THERE 
-	- ONCE OUT OF THE LOOP JUST DO THE CODE THAT PRINTS THE TOKENS FROM THE FILE.
-
-*/
 import java.io.*;
 import java.util.*;
 public class Exercise1
 {
-	public static void main( String args[] ) 
-	{
-		if (args.length < 1)
-		{
-			System.out.println("\nYou must enter an input filename on cmd line!\n");
-			System.exit(0);
-		}
-		
-		
-		Scanner infile = null;
-		Scanner kbd = new Scanner(System.in);3
-		boolean done=false;
-		while(!done)
-		{	// MODIFY, REPLACE, ADD LOOP CODE, ADD TRY CATCH BLOCK(S) AS NEEDED BELOW
-		try 
-		{
-			Scanner infile = new Scanner( new File( args[0] ) );
-		} 
-		catch (FileNotFoundException fnfe) 
-		{
-			System.out.print("File not found. Try again: ");
-			args[0] = kbd.next();
-		}
-		catch (Exception e)
-		{
-			System.out.println("Unknown exception. Aborting now!")
-			System.exit(0);
-		}
-	}
-		
-		
+    public static void main( String args[] )
+    {
+        if (args.length < 1)
+        {
+            System.out.println("\nYou must enter an input filename on cmd line!\n");
+            System.exit(0);
+        }
 
-		// THE CODE BELOW IS -AFTER, BELOW AND OUTSIDE OF- THE ABOVE LOOP 
-		
-		while (infile.hasNext())
-		{
-			String token = infile.next(); // read a string from infile
-			System.out.printf("%s\n", token);
-		}
-	}
+
+        Scanner infile = null;
+        Scanner kbd = new Scanner(System.in);
+        boolean done=false;
+        while(!done)
+        {   // MODIFY, REPLACE, ADD LOOP CODE, ADD TRY CATCH BLOCK(S) AS NEEDED BELOW
+            try
+        {
+            infile = new Scanner( new File( args[0] ) );
+            done = true;
+        }
+        catch (FileNotFoundException fnfe)
+        {
+            System.out.print( args[0] + " not found. Enter valid filename: ");
+            args[0] = kbd.next();
+        }
+        catch (Exception e)
+        {
+            System.out.println("Unknown exception. Aborting now!");
+            System.exit(0);
+        }
+    }
+
+
+
+        // THE CODE BELOW IS -AFTER, BELOW AND OUTSIDE OF- THE ABOVE LOOP
+
+        while (infile.hasNext())
+        {
+            String token = infile.next(); // read a string from infile
+            System.out.printf("%s\n", token);
+        }
+    }
 } //END CLASS
--- Lab8/Exercise2.java
+++ Lab8/Exercise2.java
@@ -1,23 +1,37 @@
-/*
-	Exercise2.java
-	- WRITE A SINGLE DO LOOP THAT DOES THE FOLLOWING UNTIL THE USER ENTERS AN INT IN 1..100 INCLUSIVE
-	- 	USE TRY CATCH TO READ AN INT FROM THE KBD SUCH THAT IF USER ENTERS "FOO" IT DOES NOT CRASH
-	- 	BE SURE TO MANUALLY THROW AN EXCEPTION & CATCH IT FOR THE CASE OF USER ENTERING AN VALID 
-	-	INT THAT HAPPENS TO BE OUT OF RANGE.
-
-*/
 import java.io.*;
 import java.util.*;
 
 public class Exercise2
 {
-	public static void main( String args[] )
-	{
-		// MODIFY, REPLACE, ADD LOOP CODE, ADD TRY CATCH BLOCK(S) AS NEEDED BELOW
-		
-		Scanner kbd = new Scanner(System.in);
-		System.out.print("Enter int in range 1..100 inclusive: ");
-		System.out.format("Thank you. You entered %d\n", kbd.nextInt() );
-		
-	} //END main
+    public static void main( String args[] )
+    {
+        int num;
+        boolean done = false;
+        Scanner kbd = new Scanner( System.in);
+        while(!done)
+        {
+            try
+            {
+                System.out.print("Enter int in range 1..100 inclusive:");
+                num = kbd.nextInt();
+                if(num <1 || num > 100) throw new Exception( "Number: " + num + " out of range." + " Must be in 1..100");
+                System.out.format("Thank you. You entered %d\n", num);
+                done = true;
+            }
+            catch(InputMismatchException ime)
+            {
+                System.out.println("Input was not an integer");
+                kbd.nextLine();
+            }
+            catch(Exception e)
+            {
+                String msg = "" + e;
+                msg = msg.replaceFirst("java.lang.Exception: ","");
+                System.out.println(msg);
+                kbd.nextLine();
+            }
+        }
+        // MODIFY, REPLACE, ADD LOOP CODE, ADD TRY CATCH BLOCK(S) AS NEEDED BELOW
+
+    }//END main
 } //END CLSS
